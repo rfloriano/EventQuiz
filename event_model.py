404591d2581d752da79a01a1d7630193594d2ec5
Felipe Ramos (Cabuto) <felipe.ramos@corp.globo.com>
2013-10-02 21:57:07 -0300

    Logout working

---
+++ event_model.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+def auth_user(user):
+    if user == 'test':
+        return True
+
+    return False
--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request, redirect, url_for, make_respo
 from flask.ext.assets import Environment
 
 from event_model import auth_user
+from helpers import current_user, set_current_user
 
 app = Flask(__name__)
 assets = Environment(app)
@@ -12,9 +13,7 @@ assets = Environment(app)
 
 @app.route('/')
 def index():
-    user = request.cookies.get('user')
-
-    if not user:
+    if not current_user():
         return redirect(url_for('login'))
 
     return render_template('index.html')
@@ -23,11 +22,20 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST' and auth_user(request.form.get('login')):
-        resp = make_response(redirect(url_for('index')))
-        resp.set_cookie('user', request.form.get('login'))
-        return resp
+        return set_current_user(request.form.get('login'))
 
     return render_template('login.html')
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    return set_current_user('')
+    #return render_template('logout.html')
+
+
+@app.context_processor
+def utility_processor():
+    return dict(current_user=current_user)
 
 
 if __name__ == '__main__':
