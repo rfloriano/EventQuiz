101ee802a0abb359e6f0f978b909d2744489260e
Rafael Floriano da Silva <rflorianobr@gmail.com>
2013-10-03 14:58:28 -0300

    Added admin.py and fixes answer string

--- quiz/quiz/question/models.py
+++ quiz/quiz/question/models.py
@@ -4,8 +4,14 @@ from django.db import models
 class Question(models.Model):
     question = models.CharField(max_length=255)
 
+    def __unicode__(self):
+        return self.question
+
 
 class Answer(models.Model):
-    anwaer = models.CharField(max_length=255)
+    answer = models.CharField(max_length=255)
     is_right = models.BooleanField(default=False)
     question = models.ForeignKey(Question)
+
+    def __unicode__(self):
+        return self.answer
